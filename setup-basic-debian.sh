1b6e94321d43e98d472f83581362c6f64a5390f9
jaime-e <jechegarayz@gmail.com>
2014-06-23 14:22:00 +0000

    basic debian environment

---
+++ setup-basic-debian.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# simple dev environment for debian
+
+sudo apt-get -y install emacs
+
+
+# git pull and install dotfiles as well
+cd $HOME
+if [ -d ./dotfiles/ ]; then
+    mv dotfiles dotfiles.old
+fi
+if [ -d .emacs.d/ ]; then
+    mv .emacs.d .emacs.d~
+fi
+git clone https://github.com/jaime-e/dotfiles.git
+ln -sb dotfiles/.screenrc .
+ln -sb dotfiles/.bash_profile .
+ln -sb dotfiles/.bashrc .
+ln -sb dotfiles/.bashrc_custom .
+ln -sf dotfiles/.emacs.d .
