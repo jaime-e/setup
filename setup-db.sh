6a67ffa9428b68ab25076a14dc3a0fce1824e465
jaime-e <jechegarayz@gmail.com>
2014-06-25 19:03:57 +0000

    postgres setup

---
+++ setup-db.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# postgres db setup
+
+
+#poner como editar postgres.conf para cambiar los postgre.conf para cambiar la ip de escucha
+cd /etc/postgresql/9.1/main/
+sudo chmod 777 postgresql.conf pg_hba.conf
+sudo sed -i '59 s/#//' postgresql.conf
+sudo sed -i '59 s/localhost/*/' postgresql.conf
+sudo sed -i '92 s+127.0.0.1/32+0.0.0.0/0+' pg_hba.conf
+sudo sed -i '94 s+::1/128+::/0+' pg_hba.conf
+
