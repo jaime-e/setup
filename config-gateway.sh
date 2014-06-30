c9769847a811cc3be2c6a4cde1e8de4555ce39a7
jaime-e <jechegarayz@gmail.com>
2014-06-30 19:15:47 +0000

    ajustar

--- config-gateway.sh
+++ config-gateway.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # ripple gateway config
 
+sudo -u postgres createdb ripple_gateway
+
 cd node_modules/ripple-gateway/config
 sudo sed -i '8 s+localhost+108.59.85.231+' config.js
-sudo sed -i '9 s+password+pass+' config.js
-sudo sed -i '9 s+localhost+108.59.85.231+' config.js
-sudo sed -i '15 s+localhost+108.59.85.231+' config.js
+sudo sed -i '11 s+password+pass+' config.js
+sudo sed -i '11 s+localhost+108.59.85.231+' config.js
+sudo sed -i '23 s+localhost+108.59.85.231+' config.js
 
 cd ..
-ln -s ~/node_modules/ripple-gateway/lib/data ~/node_modules/ripple-gateway/
+ln -s ~/node_modules/ripple-gateway/lib/data/migrations ~/node_modules/ripple-gateway/migrations
 
 cd $HOME
 export DATABASE_URL=postgres://postgres:pass@108.59.85.231:5432/ripple_gateway?native=true
