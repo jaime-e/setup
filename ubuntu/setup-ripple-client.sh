363585485db8490017d19738b066e5c8c58782bb
jaime-e <jechegarayz@gmail.com>
2014-08-05 22:10:54 +0000

    archivos para ubuntu

---
+++ ubuntu/setup-ripple-client.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# ripple gateway setup
+
+#instalar python, g++, make, libpq-dev, nodejs, grunt, npm
+
+sudo apt-get -y install python-software-properties
+sudo apt-get -y install python
+sudo apt-get -y install g++
+sudo apt-get -y install make
+sudo apt-get -y install libpq-dev
+sudo add-apt-repository -y ppa:chris-lea/node.js
+sudo apt-get -y update
+sudo apt-get -y install nodejs
+sudo apt-get -y install npm
+sudo npm install -g grunt-cli
+
+#instalar cliente
+git clone https://github.com/ripple/ripple-client ./ripple-client
+cd ripple-client
+sudo npm install
+cp src/js/config-example.js src/js/config.js
+grunt
+
+#instalar servidor apache
+sudo apt-get -y install apache2
+
+
+
