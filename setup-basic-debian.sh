1184a91e94f430350d60dfdde998bdff7fd01a0d
jaime-e <jechegarayz@gmail.com>
2014-09-05 18:55:31 +0000

    agruegar ln para git promp

--- setup-basic-debian.sh
+++ setup-basic-debian.sh
@@ -17,6 +17,7 @@ ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
+ln -sb dotfiles/.git-prompt.sh .
 ln -sf dotfiles/.emacs.d .
 
 #rlwrap
