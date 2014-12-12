f9d4bc4c2099fd6627b8005ef94ddba358f3b6b3
jaime-e <jechegarayz@gmail.com>
2014-12-12 15:18:38 +0000

    reordenamiento

---
+++ install-blobvault.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#path cd home?
+
+#2: Clone repo and install dependencies
+
+git clone https://github.com/dinexcode/dnxapp-blobvault.git &&
+cd  dnxapp-blobvault
+npm install --save
+
+#knexmigration
+
